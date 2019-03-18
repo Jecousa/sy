d54f933d3c99e3c048c7febd284e4462f1dd7e87
jecousa <jecolousa@gmail.com>
2019-03-18 12:03:22 -0600

    backend setup

--- events/admin.py
+++ events/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'description')
+    list_display_links = ('id', 'title')
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -6,7 +6,9 @@ class Event(models.Model):
     date = models.DateTimeField(default=datetime.now)
     description = models.CharField(max_length=200)
     detail = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, default='')
+    is_published = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.title
+#main Field that is displayed
+def __str__(self):
+    return self.title
